62ced70e3c434013436311b832e3fb5c66b1777d
Mikhail Rudgalsky <rudgalsky@gmail.com>
2020-02-01 16:13:50 +0600

    Implement main menu.

---
+++ AppleGame/ViewModels/MainMenuViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppleGame.ViewModels
+{
+    /// <summary>
+    /// Main menu ViewModel.
+    /// </summary>
+    public class MainMenuViewModel : Screen
+    {
+        public static readonly MainMenuViewModel MainMenuInstance = new MainMenuViewModel();
+
+        public void NewGame()
+        {
+            this.TryClose(true);
+        }
+
+        public void Exit()
+        {
+            this.TryClose(false);
+        }
+    }
+}
--- AppleGame/ViewModels/ShellViewModel.cs
+++ AppleGame/ViewModels/ShellViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace AppleGame.ViewModels
 {
@@ -12,6 +13,33 @@ namespace AppleGame.ViewModels
     /// </summary>
     public class ShellViewModel : Screen
     {
+        private IWindowManager _windowManager;
 
+        /// <summary>
+        /// Main ViewModel.
+        /// </summary>
+        public ShellViewModel(IWindowManager windowManager)
+        {
+            _windowManager = windowManager;
+        }
+
+        /// <summary>
+        /// Called when activating.
+        /// </summary>
+        protected override void OnActivate()
+        {
+            // Firstly, show the main menu.
+            var mainMenu = MainMenuViewModel.MainMenuInstance;
+            Execute.OnUIThread(() => 
+            {
+                bool? dialogResult = _windowManager.ShowDialog(mainMenu);
+
+                // If user presses "Exit", then the application will be closed.
+                if (dialogResult == false)
+                    Application.Current.Shutdown();
+            });
+
+            base.OnActivate();
+        }
     }
 }
