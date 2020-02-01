fc482c9f182f6d763a900af028954faf547dc608
Mikhail Rudgalsky <rudgalsky@gmail.com>
2020-02-01 13:54:38 +0600

    Setup CaliburnMicro.

---
+++ AppleGame/AppBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using AppleGame.ViewModels;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AppleGame
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
